e15f81d3539cbb01ef4a338822d2120e7b2aae84
Jforjo <jaholmes2002@hotmail.com>
2024-05-20 09:43:04 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -3,7 +3,7 @@ import { InstallGlobalCommands } from "./utils";
 const COMMANDS = [];
 
 COMMANDS.push({
-    name: 'Slap',
+    name: 'slap',
     description: 'Sometimes you gotta slap a person with a large trout',
     type: 1,
     options: [
@@ -17,21 +17,25 @@ COMMANDS.push({
 });
 
 COMMANDS.push({
-    name: 'Invite',
+    name: 'invite',
     description: 'Get an invite link to add the bot to your server',
     type: 1,
 });
 
 COMMANDS.push({
-    name: 'Support',
+    name: 'support',
     description: 'Like this bot? Support me!',
     type: 1,
 });
 
 COMMANDS.push({
-    name: 'Ping',
+    name: 'ping',
     description: 'Ping the bot',
     type: 1,
 });
 
-InstallGlobalCommands(process.env.APPLICATION_ID, COMMANDS);
+export default async (req, res) => {
+    const status = await InstallGlobalCommands(process.env.APPLICATION_ID, COMMANDS);
+    if (status.ok) return res.json({ message: "Commands Loaded" });
+    else return res.json({ message: "Commands NOT Loaded" });
+}
