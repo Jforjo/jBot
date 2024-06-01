391ee2e1a0826a24fa34b1eebad83ad20c0b512c
Jforjo <jaholmes2002@hotmail.com>
2024-06-01 18:11:15 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -3,23 +3,23 @@ import { InstallGlobalCommands, DeleteGlobalCommands } from "../utils";
 
 const COMMANDS = [];
 
-COMMANDS.push({
-    name: 'invite',
-    description: 'Get an invite link to add the bot to your server',
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-});
+// COMMANDS.push({
+//     name: 'invite',
+//     description: 'Get an invite link to add the bot to your server',
+//     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+// });
 
-COMMANDS.push({
-    name: 'support',
-    description: 'Like this bot? Support me!',
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-});
+// COMMANDS.push({
+//     name: 'support',
+//     description: 'Like this bot? Support me!',
+//     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+// });
 
-COMMANDS.push({
-    name: 'ping',
-    description: 'Replies with Pong!',
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-});
+// COMMANDS.push({
+//     name: 'ping',
+//     description: 'Replies with Pong!',
+//     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+// });
 
 COMMANDS.push({
     name: 'clicker',
